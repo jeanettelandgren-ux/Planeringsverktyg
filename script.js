2b98aad26246652d13ece9ad4ec21cd8a4cab611
jeanettelandgren-ux <jeanette.landgren@gmail.com>
2025-09-11 12:03:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-function showGreeting() {
-  const name = localStorage.getItem("userName");
+document.getElementById("menu-resurser").addEventListener("click", () => {
+  window.location.href = "resurser.html";
+});
 
-  if (!name) {
-    const enteredName = prompt("Välkommen! Vad heter du?");
-    if (enteredName && enteredName.trim() !== "") {
-      localStorage.setItem("userName", enteredName.trim());
-      document.getElementById("greeting").innerText = "Välkommen!";
-    } else {
-      document.getElementById("greeting").innerText = "Välkommen!";
-    }
-  } else {
-    document.getElementById("greeting").innerText = `Välkommen tillbaka, ${name}!`;
-  }
-}
-
-window.onload = showGreeting;
+document.getElementById("menu-projekt").addEventListener("click", () => {
+  window.location.href = "ny-artikel.html";
+});
