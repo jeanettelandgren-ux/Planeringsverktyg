4dd766395e0e3224b4619ddc57ff4dad61ff2510
jeanettelandgren-ux <jeanette.landgren@gmail.com>
2025-09-11 12:43:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
+document.getElementById("menu-planering").addEventListener("click", () => {
+  window.location.href = "planering.html";
+});
+
+document.getElementById("menu-artiklar").addEventListener("click", () => {
+  window.location.href = "artiklar.html";
+});
+
+document.getElementById("menu-ny-artikel").addEventListener("click", () => {
+  window.location.href = "ny-artikel.html";
+});
+
+document.getElementById("menu-order").addEventListener("click", () => {
+  window.location.href = "order.html";
+});
+
+document.getElementById("menu-ny-order").addEventListener("click", () => {
+  window.location.href = "ny-order.html";
+});
+
 document.getElementById("menu-resurser").addEventListener("click", () => {
   window.location.href = "resurser.html";
 });
 
-document.getElementById("menu-projekt").addEventListener("click", () => {
-  window.location.href = "ny-artikel.html";
+document.getElementById("menu-kalender").addEventListener("click", () => {
+  window.location.href = "kalender.html";
 });
